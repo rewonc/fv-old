711aa8871f5859b656228c1bdf5b282a2a5f9d0c
rewonc <rewonc@gmail.com>
2013-10-07 18:38:21 -0400

    one more save

--- spec/factories/attributes.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :attribute do
-    product nil
-    options "MyString"
-    title "MyString"
-  end
-end
