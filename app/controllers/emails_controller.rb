0c8b07056c7d8e08329f068dc1dbe6195226656e
rewonc <rewonc@gmail.com>
2013-10-10 22:10:51 -0400

    finished landing page, basically..

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,24 @@
 class EmailsController < ApplicationController
+		http_basic_authenticate_with name: "orange1", password: "orangutan", only: [:index]
+
 
 	def create
+	  @email = Email.new(email_params)
+	  	if @email.save
+		  flash[:notice] = 'Thanks for your input. We will contact you shortly.'
+		  redirect_to root_path 
+		else
+			flash[:error] = 'Sorry, your data was invalid. Please try again. '
+		  redirect_to root_path 
+		end		
 	end
+
+	def index
+		@emails = Email.all
+	end
+
+	private 
+	def email_params
+		params.require(:email).permit(:name, :email, :zip)
+	end 
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,5 @@
 class Email < ActiveRecord::Base
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :zip, presence: true
 end
