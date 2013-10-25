3bbb7fa7a9a27728af3b6c96359e1b9ae690d085
rewonc <rewonc@gmail.com>
2013-10-24 22:37:08 -0400

    responds now

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -4,7 +4,8 @@ skip_before_filter :verify_authenticity_token
 
     message_body = params["Body"]
     from_number = params["From"]
-
+    @fromcity = params["FromCity"]
+    @number = from_number
     SmsLogger.log_text_message(from_number, message_body)
   end
 
