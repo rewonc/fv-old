9f2135ff7fcd379df3449d1f248e1994f7a807b1
rewonc <rewonc@gmail.com>
2013-10-24 22:53:34 -0400

    small edit

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -5,7 +5,15 @@ skip_before_filter :verify_authenticity_token
     message_body = params["Body"]
     from_number = params["From"]
     @fromcity = params["FromCity"]
-    @number = from_number
+    if from_number == "17754121514"
+    	@name = "Rewon"
+    elsif from_number == "15707987565"
+    	@name = "Sherwin"
+    elsif from_number == "19018250094"
+    	@name = "Ray"
+    else
+    	@name = "Good ol... what was your name again?"
+    end
     SmsLogger.log_text_message(from_number, message_body)
     render :layout => false
   end
