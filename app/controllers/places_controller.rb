82c3b2027f055ccba9acf49d339353e0f31b3d52
rewonc <rewonc@gmail.com>
2013-10-30 17:19:05 -0400

    preupload of demo

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,30 +19,7 @@ class PlacesController < ApplicationController
 
   def show
   	@place = Place.find(params[:id])
-    #will add in these functions later, for now will hardcode
-    #week = @place.week.current
-    #lastweek = @place.week.last
-    #views will call these functions later, but for now we hardcode and pass them from the controller
-    #@weekfarms => Farm.farms_listing_this_week
-    #each of those farms, in addition to their normal information, has an array of products, which are called by 
-    #  Farm.products_at_market_this_week(week, market) should replace what we're doing right now, which is just farm.products
-    weekfarms = Farm.all 
-
-    #assume week is inactive for now (>24 hours before market open?)
-    @week = {day: "Saturday", date: "November 2, 2013", market: @place, farms: weekfarms, active: false}
-
-    lastweekfarms = Farm.all 
- 
-    @lastweek = {day: "Saturday", date: "October 26, 2013", market: @place, farms: lastweekfarms, active: false}
-
-
-
-    #model validation notes for later:
-    #have a date/week object (belongs to market). Each date object has many products. Products belong to farms.
-    #So, the controller should pass the active date object for this week, along with some logic which determines whether
-    #it should be hidden or shown.
-    #We need some model methods: farms_listing_this_week(market) + products_for_sale_this_week(farm, market)
-    #The date object will go through and build the farm/product table for each farm that lists.
+    
   end
 
   def destroy
