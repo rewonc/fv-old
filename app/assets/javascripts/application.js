880bb80e5feddad1bcb13f00d12c41ef309aaf2d
rewonc <rewonc@gmail.com>
2013-10-30 22:28:52 -0400

    added demo data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,17 +53,17 @@ $(function() {
 	});
 });
 
-$("#search-lastweek").keyup(function() {
+$("#search-lastweek").keyup(function () {
     var value = this.value.toLowerCase().trim();
 
-	$(".sortable").find("tr").each(function(index) {
-            if (!index) return;
-            $(this).find("td").each(function(){
-            	var id = $(this).text().toLowerCase().trim();
-            	var not_found = (id.indexOf(value) == -1);
-            	$(this).closest('tr').toggle(!not_found);
-            	return not_found;
-            });
+    $(".sortable").find("tr").each(function (index) {
+        if (!index) return;
+        $(this).find("td").each(function () {
+            var id = $(this).text().toLowerCase().trim();
+            var not_found = (id.indexOf(value) == -1);
+            $(this).closest('tr').toggle(!not_found);
+            return not_found;
+        });
     });
 });
 
