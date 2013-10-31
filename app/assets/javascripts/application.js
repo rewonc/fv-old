14730a878c07c6dd79650b9f6e5579f6c15616e9
rewonc <rewonc@gmail.com>
2013-10-30 21:22:00 -0400

    sortable menu and search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,11 +49,21 @@ $(function() {
             
             inverse = !inverse;
                 
-        });
-		                
+        });                
 	});
-
-
-
-
 });
+
+$("#search-lastweek").keyup(function() {
+    var value = this.value.toLowerCase().trim();
+
+	$(".sortable").find("tr").each(function(index) {
+            if (!index) return;
+            $(this).find("td").each(function(){
+            	var id = $(this).text().toLowerCase().trim();
+            	var not_found = (id.indexOf(value) == -1);
+            	$(this).closest('tr').toggle(!not_found);
+            	return not_found;
+            });
+    });
+});
+
