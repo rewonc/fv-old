63911932ecfa5f42f5bd8967229048c745c86e3b
rewonc <rewonc@gmail.com>
2013-10-30 20:15:50 -0400

    added sortelements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,46 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require sortelements
 //= require_tree .
 
 $(document).foundation();
+
+$(function() {
+	
+	$( ".sortable" ).find('th')
+
+	.wrapInner('<span title="sort this column"/>')
+
+	.each(function(){
+		var th = $(this),
+      	thIndex = th.index(),
+        inverse = false,
+        table = th.closest('table');
+
+		th.click(function(){
+            table.find('td').filter(function(){
+                return $(this).index() === thIndex;
+            }).sortElements(function(a, b){
+                
+                return $.text([a]) > $.text([b]) ?
+                    inverse ? -1 : 1
+                    : inverse ? 1 : -1;
+                
+            }, function(){
+               
+                // parentNode is the element we want to move
+                return this.parentNode; 
+                
+            });
+            
+            inverse = !inverse;
+                
+        });
+		                
+	});
+
+
+
+
+});
