143f643f1c90ed07252f5723da48117bdb3550e6
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-11-15 13:29:36 -0500

    Landing page styling: finish introduction div

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131114173540) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "boxes", force: true do |t|
     t.integer  "box_num"
     t.string   "firstname"
