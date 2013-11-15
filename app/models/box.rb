0e04a36b5ec5037568bdf48c1bccabbe025133d0
rewonc <rewonc@gmail.com>
2013-11-15 11:14:09 -0500

    amount must be at least 50 cents...

--- app/models/box.rb
+++ app/models/box.rb
@@ -42,13 +42,13 @@ class Box < ActiveRecord::Base
   def get_price
     #add 400 for delivery
     if box_num == 1 
-        return 25;
+        return 50;
       elsif box_num == 2
-        return 25;
+        return 50;
       elsif box_num == 3
-        return 25;
+        return 50;
       else
-        return 25;
+        return 50;
       end
   end
 
