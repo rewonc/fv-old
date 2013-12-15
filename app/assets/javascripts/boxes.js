5e5843e264f33fe486ca1504aca1aa02495c7cc0
rewonc <rewonc@gmail.com>
2013-12-15 17:17:55 -0500

    got slider to work w/ tabs, price updates, and content changes

--- app/assets/javascripts/boxes.js
+++ app/assets/javascripts/boxes.js
@@ -1,32 +1,63 @@
-$( "#confirm_button" ).click(function() {
-  $( "#confirm_box" ).hide();
-  $("#payment_box").fadeIn();
-});
-
 $(".no-js-desc").hide();
-sliders("#box_raw_num",1);
-sliders("#box_cook_num",2);
-sliders("#box_fruit_num",3);
+sliders("#box_raw_num");
+sliders("#box_cook_num");
+sliders("#box_fruit_num");
+tabsSelect();
 
-function sliders(selector, module) {
-    var select = $( selector ).hide();
+function tabsSelect(){
+  $('.tabs').find('dd').click(function(){
+    var selection = $(this).data('selection');
+    $('#box_box_num_' + selection).prop("checked",true);
+    switch(selection)
+    {
+      case 1:
+        setModules(2,0,1);
+        break;
+      case 2:
+        setModules(1,1,1);
+        break;
+      case 3:
+        break;
+    }
+  });
+}
+
+function setModules(raw,cook,fruit){
+  $('#box_raw_num').val(raw).selectToSlider();
+  $('#box_cook_num').val(cook).selectToSlider();
+  $('#box_fruit_num').val(fruit).selectToSlider();
+  upgrade_price();
+}
+
+function sliders(selector) {
+    var select = $( selector )//.hide();
     var slider = $( "<div class='slider'></div>" ).insertAfter( select ).slider({
       min: 0,
       max: 3,
       value: select[ 0 ].selectedIndex,
       slide: function( event, ui ) {
         select[ 0 ].selectedIndex = ui.value;
-        upgrade_text(module,ui.value);
+        upgrade_text(select, ui.value);
+        upgrade_price();
       }
     });
 
-    $( "#box_raw_num" ).change(function() {
-      slider.slider( "value", this.selectedIndex);
-      upgrade_text(module,this.selectedIndex);
+    $( select ).change(function() {
+      $(this).selectToSlider(); 
+      upgrade_price();
     });
 }
 
-function upgrade_text(module, value){
+jQuery.fn.selectToSlider = function(){
+  $(this).next(".slider").slider("value", this[0].selectedIndex);
+  upgrade_text(this, this[0].selectedIndex);
+};
+
+function upgrade_price(){
+  $("#price").append(' <-');
+}
+function upgrade_text(select, value){
+  module = $(select).closest('li').data('module');
   switch(module)
     {
     case 1:
