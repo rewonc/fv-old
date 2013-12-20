3e825701e2f59c4c562d10021b9cc8afeed27159
rewonc <rewonc@gmail.com>
2013-12-20 15:05:17 -0500

    redoing assets on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -42,7 +42,7 @@ Farmivore::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.01'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
