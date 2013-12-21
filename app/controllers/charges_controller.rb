30ac29b1baf9e42f71ae3252174b93f545211000
rewonc <rewonc@gmail.com>
2013-12-21 05:44:48 -0500

    adding social media

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,18 +8,18 @@ class ChargesController < ApplicationController
     # https://stripe.com/docs/tutorials/charges  <-- on how to store the token and charge it later
     @box = Box.find(session[:box_id])
     amount = @box.get_price
-    #@customer = Stripe::Customer.create(
-    #  email: @box.email,
-    #  description: 'Modules:' + @box.raw_num.to_s + '-' + @box.cook_num.to_s + @box.fruit_num.to_s + '. Deliver: ' + @box.startdate.to_s + '. Every: ' + @box.frequency.to_s + 'weeks',
-    #  card: params[:stripeToken]
-    #)
+    @customer = Stripe::Customer.create(
+      email: @box.email,
+      description: 'Modules:' + @box.raw_num.to_s + '-' + @box.cook_num.to_s + @box.fruit_num.to_s + '. Deliver: ' + @box.startdate.to_s + '. Every: ' + @box.frequency.to_s + 'weeks',
+      card: params[:stripeToken]
+    )
 
-    #@charge = Stripe::Charge.create(
-    #  customer: @customer.id,
-    #  amount: amount,
-    #  description: @box.email.to_s,
-    #  currency: 'usd'
-    #)
+    @charge = Stripe::Charge.create(
+      customer: @customer.id,
+      amount: amount,
+      description: @box.email.to_s,
+      currency: 'usd'
+    )
     render 'charges/create', layout: 'application'
 
   rescue Stripe::CardError => e
