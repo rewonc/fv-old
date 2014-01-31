45296e060fa0f4888e092a8e8abc2019f175d68f
JOSE <jose@ubuntu.(none)>
2014-01-31 12:17:43 -0500

    jose's changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127163402) do
+ActiveRecord::Schema.define(version: 20140127205647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,14 @@ ActiveRecord::Schema.define(version: 20140127163402) do
   create_table "sms_loggers", force: true do |t|
     t.string   "body"
     t.string   "from"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_zips", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "login"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
