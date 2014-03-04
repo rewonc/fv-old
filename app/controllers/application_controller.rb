67fe7bcaefea9fee8a97dd7486e1de3bb976610d
rewonc <rewonc@gmail.com>
2014-03-04 13:50:30 -0500

    changign stuff around

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-
-  before_action :configure_devise_permitted_parameters, if: :devise_controller?
   before_filter :capture_referral
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  
 
   private
   def capture_referral
