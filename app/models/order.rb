bd3607c06eba686875604402d3f9f2d384cef7f7
rewonc <rewonc@gmail.com>
2014-03-07 09:23:53 -0500

    updating order page

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,13 @@
 class Order < ActiveRecord::Base
   validates :box_count, presence: true
-  validates :frequency, presence: true
+  #validates :frequency, presence: true
   validates :fullname, presence: true
   validates :address1, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :zip, presence: true
   validates :phone, presence: true
-  validates :delivery_window, presence: true
+  #validates :delivery_window, presence: true
   validates :first_delivery, presence: true
   belongs_to :user
   belongs_to :product
