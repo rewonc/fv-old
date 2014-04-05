6ec3af4530e18ed05ed88eacdc6dfb4f054ae7e8
rewonc <rewonc@gmail.com>
2014-04-05 14:29:28 -0400

    landing page redesign

--- app/controllers/delivery_controller.rb
+++ app/controllers/delivery_controller.rb
@@ -1,2 +1,20 @@
 class DeliveryController < ApplicationController
+
+  def activate_order(order)
+
+    order.active = true
+    
+    respond_to do |format|
+      if order.save
+        format.json { render action: 'show', status: :created, location: @order }
+      else
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def activate_order(order)
+    #order.
+  end
+
 end
