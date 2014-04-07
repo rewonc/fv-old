05dc29d85cf8f99ed6c7db4116b00f1e429589ab
rewonc <rewonc@gmail.com>
2014-04-06 23:16:33 -0400

    trying to fix devise mailer

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -31,6 +31,9 @@ Farmivore::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
+  #for the devise mailer
+  config.action_mailer.default_url_options = { :host => 'farmivore-staging.heroku.com' }
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
